28cbc95a4f6dd8475a2793e4b54b7895c2d053d4
DishaK0815 <dishakhirade0815@gmail.com>
2025-10-04 13:10:35 +0530

    Add files via upload

---
+++ Python/33_reverse_pair.py
@@ -0,0 +1,49 @@
+class Solution(object):
+    def reversePairs(self, nums):
+        def merge(arr, low, mid, high):
+            temp = []
+            left = low
+            right = mid + 1
+
+            while left <= mid and right <= high:
+                if arr[left] <= arr[right]:
+                    temp.append(arr[left])
+                    left += 1
+                else:
+                    temp.append(arr[right])
+                    right += 1
+
+            while left <= mid:
+                temp.append(arr[left])
+                left += 1
+
+            while right <= high:
+                temp.append(arr[right])
+                right += 1
+
+            for i in range(low, high + 1):
+                arr[i] = temp[i - low]
+
+        def countPairs(arr, low, mid, high):
+            right = mid + 1
+            cnt = 0
+            for i in range(low, mid + 1):
+                while right <= high and arr[i] > 2 * arr[right]:
+                    right += 1
+                cnt += (right - (mid + 1))
+            return cnt
+
+        def mergeSort(arr, low, high):
+            if low >= high:
+                return 0
+            mid = (low + high) // 2
+            cnt = mergeSort(arr, low, mid)
+            cnt += mergeSort(arr, mid + 1, high)
+            cnt += countPairs(arr, low, mid, high)
+            merge(arr, low, mid, high)
+            return cnt
+
+        return mergeSort(nums, 0, len(nums) - 1)
+nums = [1,3,5,3,1]
+sol = Solution()
+print(sol.reversePairs(nums))
