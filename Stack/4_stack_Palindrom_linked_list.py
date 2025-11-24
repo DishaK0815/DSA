c14c1368ced5bd81463afb0d38ce6c0004c0f635
DishaK0815 <dishakhirade0815@gmail.com>
2025-11-24 17:31:50 +0530

    Add files via upload

---
+++ Stack/4_stack_Palindrom_linked_list.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        stack = []
+        curr = head
+        while curr:
+            stack.append(curr.val)
+            curr = curr.next
+        curr = head
+        while curr:
+            if curr.val != stack.pop():
+                return False
+            curr = curr.next
+        return True
+       
+        
+        
